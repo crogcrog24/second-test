1245e31cd8d70f657280e09194cc6b9f4124e09b
Arnimesh <arnimeshk508@gmail.com>
2021-02-12 21:30:03 +0530

    2nd commit

---
+++ new.js
@@ -0,0 +1,6 @@
+var date = new Date();
+function createDateAsUTC(date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+}
+console.log(date);
+console.log('dsfsfdk');
